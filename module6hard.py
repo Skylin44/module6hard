cf6422842f3d6188076df64d6925015956aa853a
Елена Владимировна <50554917+Skylin44@users.noreply.github.com>
2024-10-27 12:02:57 +0300

    Add files via upload

---
+++ module6hard.py
@@ -0,0 +1,134 @@
+
+# Атрибуты класса Figure: sides_count = 0
+# Каждый объект класса Figure должен обладать следующими атрибутами:
+# Атрибуты(инкапсулированные): __sides(список сторон (целые числа)), __color(список цветов в формате RGB)
+# Атрибуты(публичные): filled(закрашенный, bool)
+# И методами:
+# Метод get_color, возвращает список RGB цветов.
+# Метод __is_valid_color - служебный, принимает параметры r, g, b, который проверяет корректность переданных значений перед установкой нового цвета. Корректным цвет: все значения r, g и b - целые числа в диапазоне от 0 до 255 (включительно).
+# Метод set_color принимает параметры r, g, b - числа и изменяет атрибут __color на соответствующие значения, предварительно проверив их на корректность. Если введены некорректные данные, то цвет остаётся прежним.
+# Метод __is_valid_sides - служебный, принимает неограниченное кол-во сторон, возвращает True если все стороны целые положительные числа и кол-во новых сторон совпадает с текущим, False - во всех остальных случаях.
+# Метод get_sides должен возвращать значение я атрибута __sides.
+# Метод __len__ должен возвращать периметр фигуры.
+# Метод set_sides(self, *new_sides) должен принимать новые стороны, если их количество не равно sides_count, то не изменять, в противном случае - менять.
+#
+# Атрибуты класса Circle: sides_count = 1
+# Каждый объект класса Circle должен обладать следующими атрибутами и методами:
+# Все атрибуты и методы класса Figure
+# Атрибут __radius, рассчитать исходя из длины окружности (одной единственной стороны).
+# Метод get_square возвращает площадь круга (можно рассчитать как через длину, так и через радиус).
+#
+# Атрибуты класса Triangle: sides_count = 3
+# Каждый объект класса Triangle должен обладать следующими атрибутами и методами:
+# Все атрибуты и методы класса Figure
+# Метод get_square возвращает площадь треугольника. (можно рассчитать по формуле Герона)
+# Атрибуты класса Cube: sides_count = 12
+# Каждый объект класса Cube должен обладать следующими атрибутами и методами:
+# Все атрибуты и методы класса Figure.
+# Переопределить __sides сделав список из 12 одинаковы сторон (передаётся 1 сторона)
+# Метод get_volume, возвращает объём куба.
+
+import math
+from math import pi, sqrt
+
+class Figure:
+    sides_count = 0
+
+    def __init__(self, filled=False, *sides):
+        self.__sides = list(sides)
+        self.__color = [0, 0, 0]
+        self.filled = filled
+
+    def get_color(self):
+        return self.__color
+
+    def __is_valid_color(self, r, g, b):
+        return all(0 <= i <= 255 for i in [r, g, b])
+
+    def set_color(self, r, g, b):
+        if self.__is_valid_color(r, g, b):
+            self.__color = [r, g, b]
+
+    def __is_valid_sides(self, *new_sides):
+        return len(new_sides) == self.sides_count and all(isinstance(side, int) and side > 0 for side in new_sides)
+
+    def get_sides(self):
+        return self.__sides
+
+    def __len__(self):
+        return sum(self.__sides)
+
+    def set_sides(self, *new_sides):
+        if self.__is_valid_sides(*new_sides):
+            self.__sides = list(new_sides)
+
+
+class Circle(Figure):
+    sides_count = 1
+
+
+def __init__(self, color, *sides):
+    super().__init__(color, *sides)
+    self.__radius = self._Figure__sides[0] / (2 * pi)
+
+
+def get_square(self):
+    return math.pi * (self.__radius ** 2)
+
+
+class Triangle(Figure):
+    sides_count = 3
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__square = None
+
+    def get_square(self):
+        if self.__square is None:
+            a, b, c = self.__sides
+            s = (a + b + c) / 2
+            self.__square = math.sqrt(s * (s - a) * (s - b) * (s - c))
+        return self.__square
+
+
+class Cube(Figure):
+    sides_count = 12
+
+    def __init__(self, side, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__sides = [side] * self.sides_count
+        self.__volume = side ** 3
+
+    def get_volume(self):
+        return self.__volume
+
+    def set_sides(self, new_side):
+        if new_side > 0:
+            self.__sides = [new_side] * self.sides_count
+            self.__volume = new_side ** 3
+
+    def get_sides(self):
+        return self.__sides
+
+
+
+circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
+cube1 = Cube((222, 35, 130), 6)
+
+# Проверка на изменение цветов:
+circle1.set_color(55, 66, 77) # Изменится
+print(circle1.get_color())
+cube1.set_color(300, 70, 15) # Не изменится
+print(cube1.get_color())
+
+# Проверка на изменение сторон:
+cube1.set_sides(5, 3, 12, 4, 5) # Не изменится
+print(cube1.get_sides())
+circle1.set_sides(15) # Изменится
+print(circle1.get_sides())
+
+# Проверка периметра (круга), это и есть длина:
+print(len(circle1))
+
+# Проверка объёма (куба):
+print(cube1.get_volume())
